97cf2e64be7484f33d0eeb9cd7b4508c094c0395
Daniela Cifuentes <cifu0002@algonquinlive.com>
2023-05-19 13:08:53 -0400

    Adds examples for variables

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-console.log("JS file included!")
+//console.log("JS file included!") This is a single line comment
+
+/* This is a multi 
+line comment. All info is commented out.
+Shortcut is shift + alt + A, same to comment out multiple lines*/
+
+let myName = "Daniela"
+console.log (myName)
+
+myName= "Daniela Cifuentes"
+console.log (myName)
+
+const address = "Earth"
+console.log (address)
+
+let myProfession
+console.log (myProfession)
