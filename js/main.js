af42eac51586fe48010406ea4437ffcb5202548c
Daniela Cifuentes <cifu0002@algonquinlive.com>
2023-05-19 14:02:38 -0400

    Adds demo for variables and operators

--- js/main.js
+++ js/main.js
@@ -15,3 +15,23 @@ console.log (address)
 
 let myProfession
 console.log (myProfession)
+
+const myScore = [56,76,98]
+console.log (myScore)
+
+let num = 10
+num +=2
+console.log(num)
+// Will check if the values and data types match
+console.log (num === "12")
+// Will be true if the value is greater than 90
+console.log (num > 90)
+// ! in front of any expression will negate it
+console.log (num !== 90)
+
+console.log (num >= 7 && num !== 12 )
+// The || logical operator will be true if any of the conditions are met
+console.log (num >= 7 || num !== 12 )
+// The && logical operator will be true if all conditions are met
+console.log (num >= 7 && num !== 12 && myScore [0] < 98 )
+
